c8e65a2c3f7d2510623b6de6a50cafe74525fba6
srmarti6 <srmarti6@ncsu.edu>
2021-12-17 21:36:49 -0500

    initial commit

---
+++ dababybot/dababy.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+ client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  });
+
+ client.on('message', msg => {
+  if (msg.content.toUpperCase().includes('WHERE IS DABABY')) {
+    msg.reply(('Bitch I\'m DaBaby!'));
+  } else if (msg.content.toUpperCase().includes('DABABY')) {
+    msg.reply('LETS GOOOOO');
+  }
+  });
+
+  client.on('message', msg => {
+    if (msg.content.toUpperCase().includes('DABABY PLAY')) {
+      const connection = msg.member.voice.channel;
+    }
+
+  })
+
+ client.login('ODI3NjAzMjc5MTg1MTgyODAw.YGdbnQ.5_0KPreo_AtIVU85vRJWh9OAj2k');
